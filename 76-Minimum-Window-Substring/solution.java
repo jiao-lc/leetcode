a951b036d5d762ef402729c6e7896cde6deb8411
jiao-lc <939268350@qq.com>
2016-06-02 15:15:01 +0800

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,46 +1,40 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        if(s == null || s.length() < t.length())
+        char[] s_array = s.toCharArray();
+        char[] t_array = t.toCharArray();
+        int[] map = new int[256];
+        int end = 0;
+        int start = 0;
+        int min_length = Integer.MAX_VALUE;
+        for(int i = 0; i < t_array.length; i++)
+            map[t_array[i]] ++;
+        int count = t_array.length;
+        int min_start = 0;
+        while(end < s_array.length)
+        {
+            if(map[s_array[end]] > 0)
+            {
+                count--;
+            }
+            map[s_array[end]] --;
+            while(count == 0)
+            {
+                if((end - start + 1) < min_length)
+                {
+                    min_length = end - start + 1;
+                    min_start = start;
+                }
+                map[s_array[start]] ++;
+                if(map[s_array[start]] > 0){
+                    count ++;
+                }
+                start++;
+            }
+            end ++;
+
+        }
+        if( min_start+min_length > s_array.length)
             return "";
-        int[][] charArray = new int[2][t.length()];
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        int num = 0;
-        for(int i = 0; i < t.length(); i++) {
-            int index;
-            if(!map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), num);
-                num++;
-            }
-            index = map.get(t.charAt(i));
-            charArray[0][index]++;
-        }
-        int left = 0, right = 0;
-        int len = s.length() + 1;
-        String ans = "";
-        while(right < s.length() && left < s.length() - t.length() + 1) {
-            while(num != 0 && right < s.length()) {
-                if(map.containsKey(s.charAt(right))) {
-                    int target = map.get(s.charAt(right));
-                    if(++charArray[1][target] == charArray[0][target]) {
-                        num--;
-                    }
-                }
-                right++;
-            }
-            while(num == 0 && left < right) {
-                if(len > right - left) {
-                    len = right - left;
-                    ans = s.substring(left, right);
-                }
-                if(map.containsKey(s.charAt(left))) {
-                    int target = map.get(s.charAt(left));
-                    if(--charArray[1][target] == charArray[0][target] - 1) {
-                        num++;
-                    }
-                }
-                left++;
-            }
-        }
-        return ans;
+        return s.substring(min_start, min_start+min_length);
     }
 }
