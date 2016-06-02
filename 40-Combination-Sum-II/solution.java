82b1503b068c3569f803937af498acfc9069c1af
jiao-lc <939268350@qq.com>
2016-06-02 17:26:35 +0800

    [40. Combination Sum II][Accepted]committed by LeetCode Extension

--- 40-Combination-Sum-II/solution.java
+++ 40-Combination-Sum-II/solution.java
@@ -1,23 +1,22 @@
 public class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if(candidates.length == 0)  return res;
-        Arrays.sort(candidates);
-        helper(candidates, res, new ArrayList<Integer>(), target, 0);
-        return res;
-    }
-    
-    public void helper(int[] nums, List<List<Integer>> res, List<Integer> list, int target, int start) {
-        if(target == 0) {
-            res.add(new ArrayList<Integer>(list));
-            return;
-        }
-        if(target < 0)  return;
-        for(int i = start; i < nums.length; i++) {
-            if(i > start && nums[i] == nums[i - 1]) continue;
-            list.add(nums[i]);
-            helper(nums, res, list, target - nums[i], i + 1);
-            list.remove(list.size() - 1);
+
+public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    return helper(candidates, target, 0);
+}
+List<List<Integer>> helper(int []candidates, int target, int start){
+    List<List<Integer>> ret = new ArrayList<List<Integer>>();
+    if(target == 0)
+        ret.add(new ArrayList<Integer>());
+    for(int i=start; i<candidates.length && candidates[i] <= target; i++){
+        if(i != start && candidates[i] == candidates[i-1])
+            continue;
+        List<List<Integer>> temp = helper(candidates, target - candidates[i], i+1);
+        for(List<Integer> lst: temp){
+            lst.add(0, candidates[i]);
+            ret.add(lst);
         }
     }
+    return ret;
+}
 }
