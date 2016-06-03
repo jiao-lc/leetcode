34129ce887431011a6dcc7751f2b96a101ee5f71
jiao-lc <939268350@qq.com>
2016-06-03 10:05:47 +0800

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -1,32 +1,36 @@
-public class Solution {
-    public void gameOfLife(int[][] board) {
-        if(board == null || board[0].length == 0)   return;
-        int cnt;
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                cnt = 0;
-                if(i > 0 && (board[i - 1][j] & 1) == 1) cnt++;
-                if(i < board.length - 1 && (board[i + 1][j] & 1) == 1)  cnt++;
-                if(j < board[0].length - 1 && (board[i][j + 1] & 1) == 1)  cnt++;
-                if(j > 0 && (board[i][j - 1] & 1) == 1) cnt++;
-                if(i > 0 && j > 0 && (board[i - 1][j - 1] & 1) == 1) cnt++;
-                if(j < board[0].length - 1 && i < board.length - 1 && (board[i + 1][j + 1] & 1) == 1)  cnt++;
-                if(i > 0 && j < board[0].length - 1 && (board[i - 1][j + 1] & 1) == 1) cnt++;
-                if(j > 0 && i < board.length - 1 && (board[i + 1][j - 1] & 1) == 1) cnt++;
-                
-                if((board[i][j] & 1) == 1) {
-                    if(cnt == 2 || cnt == 3) board[i][j] = 3;
-                    else board[i][j] = 1;
+ public class Solution {
+        public void gameOfLife(int[][] board) {
+            int rows=board.length;
+            int cols=board[0].length;
+            for(int i=0;i<rows;++i){
+                for(int j=0;j<cols;++j){
+                    int neighbors = getNeighbour(board, i, j);
+                    if(board[i][j]==1){
+                        if(neighbors==2 || neighbors==3)
+                            board[i][j]=3;
+                    }else{
+                        if(neighbors==3)
+                            board[i][j]=2;
+                    }
                 }
-                else {
-                    if(cnt == 3)    board[i][j] = 2;
+            }    
+            for(int i=0;i<rows;++i){
+                for(int j=0;j<cols;++j){
+                    board[i][j]>>=1;
                 }
             }
         }
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                board[i][j] = board[i][j] >> 1;
+
+        private int getNeighbour(int[][] board, int row, int col){
+            int cnt=0;
+            for(int i=row-1;i<=row+1;++i){
+                for(int j=col-1;j<=col+1;++j){
+                    if(i>=0 && i<board.length && j>=0 && j<board[0].length){
+                        cnt += board[i][j]&1;
+                    }
+                }
             }
+            cnt-=board[row][col]&1;
+            return cnt;
         }
     }
-}
