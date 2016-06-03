6e08e824506f8d06d57856dcedea07863a099cde
jiao-lc <939268350@qq.com>
2016-06-03 10:02:36 +0800

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -7,19 +7,19 @@ public class Solution {
                 cnt = 0;
                 if(i > 0 && (board[i - 1][j] & 1) == 1) cnt++;
                 if(i < board.length - 1 && (board[i + 1][j] & 1) == 1)  cnt++;
-                if(j < board.length - 1 && (board[i][j + 1] & 1) == 1)  cnt++;
+                if(j < board[0].length - 1 && (board[i][j + 1] & 1) == 1)  cnt++;
                 if(j > 0 && (board[i][j - 1] & 1) == 1) cnt++;
                 if(i > 0 && j > 0 && (board[i - 1][j - 1] & 1) == 1) cnt++;
-                if(j < board.length - 1 && i < board.length - 1 && (board[i + 1][j + 1] & 1) == 1)  cnt++;
-                if(i > 0 && j < board.length - 1 && (board[i - 1][j + 1] & 1) == 1) cnt++;
+                if(j < board[0].length - 1 && i < board.length - 1 && (board[i + 1][j + 1] & 1) == 1)  cnt++;
+                if(i > 0 && j < board[0].length - 1 && (board[i - 1][j + 1] & 1) == 1) cnt++;
                 if(j > 0 && i < board.length - 1 && (board[i + 1][j - 1] & 1) == 1) cnt++;
                 
                 if((board[i][j] & 1) == 1) {
                     if(cnt == 2 || cnt == 3) board[i][j] = 3;
-                    else board[i][j] = 2;
+                    else board[i][j] = 1;
                 }
                 else {
-                    if(cnt == 3)    board[i][j] = 1;
+                    if(cnt == 3)    board[i][j] = 2;
                 }
             }
         }
