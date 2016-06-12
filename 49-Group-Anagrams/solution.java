c49855d82d002c92e845bd4f1dbfed8528e9fe9e
jiao-lc <939268350@qq.com>
2016-06-12 16:08:58 +0800

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -1,22 +1,23 @@
 public class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<List<String>>();
-        if(strs.length == 0)    return res;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(String str : strs) {
-            char[] tmp = str.toCharArray();
-            Arrays.sort(tmp);
-            String t = new String(tmp);
-            if(!map.containsKey(t)) {
-                map.put(t, new ArrayList<String>());
+    public List<List<String>> groupAnagrams(String[] strs) { 
+        int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+
+        List<List<String>> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (String s : strs) {
+            int key = 1;
+            for (char c : s.toCharArray()) {
+                key *= prime[c - 'a'];
             }
-            List<String> ans = map.get(t);
-            ans.add(str);
-            map.put(t, ans);
-        }
-        
-        for(String key : map.keySet()) {
-            res.add(map.get(key));
+            List<String> t;
+            if (map.containsKey(key)) {
+                t = res.get(map.get(key));
+            } else {
+                t = new ArrayList<>();
+                res.add(t);
+                map.put(key, res.size() - 1);
+            }
+            t.add(s);
         }
         return res;
     }
