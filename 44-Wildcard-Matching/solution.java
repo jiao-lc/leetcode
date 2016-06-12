2fd92b258973f7e2ddf9ad2c8b64013e7e386994
jiao-lc <939268350@qq.com>
2016-06-12 13:31:31 +0800

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -2,38 +2,26 @@ public class Solution {
     public boolean isMatch(String s, String p) {
         int slen = s.length(), plen = p.length();
         if((slen == 0 && plen == 0) || p.equals("*"))   return true;
-        if(plen == 0 || slen == 0)   return false;
-        boolean[][] dp = new boolean[plen][slen];
+        boolean[][] dp = new boolean[plen + 1][slen + 1];
 
-        if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '?' || p.charAt(0) == '*') {
-            dp[0][0] = true;
-        } else {
-            return false;
+        dp[0][0] = true;
+        for(int i = 1; i < plen + 1; i++) {
+            if(p.charAt(i - 1) == '*')  dp[i][0] = dp[i - 1][0];
         }
-        if(p.charAt(0) == '*') {
-            int k = 1;
-            while(k < plen && p.charAt(k) == '*') {
-                dp[k][0] = true;
-                k++;
-            }
-            if(k < plen) {
-                if(p.charAt(k) == s.charAt(0) || p.charAt(k) == '?')  dp[k][0] = true;
-            }
-        }
-        
-        
-        for(int i = 0; i < plen; i++) {
-            for(int j = 0; j < slen; j++) {
-                if (p.charAt(i) == '*') {
-                    if( j > 0 && (dp[i][j - 1] || i > 0 && dp[i - 1][j - 1]) || i > 0 && dp[i - 1][j]) {
+        for(int i = 1; i <= plen; i++) {
+            for(int j = 1; j <= slen; j++) {
+                if (p.charAt(i - 1) == '*') {
+                    if(dp[i][j - 1] || dp[i - 1][j - 1] && i > 1 && (p.charAt(i - 2) == s.charAt(i - 1) || p.charAt(i - 2) == '?')) {
                         dp[i][j] = true;
                     }
-                } else if((p.charAt(i) == '?' || p.charAt(i) == s.charAt(j)) && j > 0 && i > 0 && dp[i - 1][j - 1]) {
+                    if(dp[i - 1][j]) {dp[i][j] = true;}
+                } 
+                if(dp[i - 1][j - 1] && (p.charAt(i - 1) == s.charAt(i - 1) || p.charAt(i - 1) == '?')){
                     dp[i][j] = true;
                 }
             }
             //if(dp[i][slen - 1]) return true;
         }
-        return dp[plen - 1][slen - 1];
+        return dp[plen][slen];
     }
 }
