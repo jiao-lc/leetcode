bf526af2a00b5579ec615d91079e72820b691e0f
jiao-lc <939268350@qq.com>
2016-06-12 15:29:39 +0800

    [274. H-Index][Accepted]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,11 +1,23 @@
 public class Solution {
-    public int hIndex(int[] citations) {
-        if(citations.length == 0)   return 0;
-        Arrays.sort(citations);
-        int i = citations.length - 1;
-        for(; i >= 0; i--) {
-            if(citations.length - i > citations[i]) break;
-        }
-        return citations.length - i - 1;
+
+public int hIndex(int[] citations) {
+    int len = citations.length;
+    int[] count = new int[len + 1];
+
+    for (int c: citations)
+        if (c > len) 
+            count[len]++;
+        else 
+            count[c]++;
+
+
+    int total = 0;
+    for (int i = len; i >= 0; i--) {
+        total += count[i];
+        if (total >= i)
+            return i;
     }
+
+    return 0;
+}
 }
