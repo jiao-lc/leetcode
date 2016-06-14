fe3e8323729cf0a38d2492e7ef826cd0a53b82bd
jiao-lc <939268350@qq.com>
2016-06-14 10:52:54 +0800

    [84. Largest Rectangle in Histogram][Accepted]committed by LeetCode Extension

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -1,18 +1,25 @@
 public class Solution {
-    public int largestRectangleArea(int[] h) {
-        int n = h.length;
-        int max = 0;
-        int[] stack = new int[n + 1];
-        int is = -1;
-        for (int i = 0; i <= n; i++) {
-            int height = (i == n) ? 0 : h[i];
-            while (is != -1 && height < h[stack[is]]) {
-                int hh = h[stack[is--]];
-                int width = (is == -1) ? i : i - 1 - stack[is];
-                max = Math.max(max, hh * width);
-            }
-            stack[++is] = i;
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) return 0;
+        return getMax(heights, 0, heights.length);
+    }    
+    int getMax(int[] heights, int s, int e) {
+        if (s + 1 >= e) return heights[s];
+        int min = s;
+        boolean sorted = true;
+        for (int i = s; i < e; i++) {
+            if (i > s && heights[i] < heights[i - 1]) sorted = false;
+            if (heights[min] > heights[i]) min = i;
         }
-        return max;
+        if (sorted) {
+            int max = 0;
+            for (int i = s; i < e; i++) {
+                max = Math.max(max, heights[i] * (e - i));
+            }
+            return max;
+        }
+        int left = (min > s) ? getMax(heights, s, min) : 0;
+        int right = (min < e - 1) ? getMax(heights, min + 1, e) : 0;
+        return Math.max(Math.max(left, right), (e - s) * heights[min]);
     }
 }
