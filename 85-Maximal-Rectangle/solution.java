69e0799beb377afe7051b9f13e325b52d17c20e8
jiao-lc <939268350@qq.com>
2016-06-14 11:28:50 +0800

    [85. Maximal Rectangle][Accepted]committed by LeetCode Extension

--- 85-Maximal-Rectangle/solution.java
+++ 85-Maximal-Rectangle/solution.java
@@ -1,48 +1,55 @@
 public class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0 || matrix[0].length == 0) return 0;
-        int m = matrix.length, n = matrix[0].length;
-        int[][] num = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == 0) {
-                    num[i][j] = matrix[i][j] - '0';
-                } else {
-                    if(matrix[i][j] == '0') {
-                        num[i][j] = 0;
-                    } else {
-                        num[i][j] = 1 + num[i - 1][j];
-                    }
-                }
-            }
-        }
-        
-        int max = 0;
-        for(int i = 0; i < m; i++) {
-            max = Math.max(max, getMax(num[i], 0, n));
-        }
-        return max;
+
+public int maximalRectangle(char[][] matrix)
+{
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+        return 0;
+
+    int[] height = new int[matrix[0].length];
+    for (int i = 0; i < matrix[0].length; i++)
+    {
+        if (matrix[0][i] == '1')
+            height[i] = 1;
     }
-    
-    public int getMax(int[] h, int s, int e) {
-        if(s + 1 >= e) {
-            return h[s];
-        }
-        int min = s;
-        boolean sorted = true;
-        for(int i = s; i < e; i++) {
-            if(i > s && h[i] < h[i - 1])    sorted = false;
-            if(h[i] < h[min])   min = i;
-        }
-        int max = 0;
-        if(sorted) {
-            for(int i = s; i < e; i++) {
-                max = Math.max(max, h[i] * (e - i));
-            }
-            return max;
-        }
-        int left = min > s ? getMax(h, s, min) : 0;
-        int right = min < e - 1 ? getMax(h, min + 1, e) : 0;
-        return Math.max(Math.max(left, right), (e - s) * h[min]);
+    int result = largestInLine(height);
+    for (int i = 1; i < matrix.length; i++)
+    {
+        resetHeight(matrix, height, i);
+        result = Math.max(result, largestInLine(height));
     }
+
+    return result;
+}
+
+private void resetHeight(char[][] matrix, int[] height, int idx)
+{
+    for (int i = 0; i < matrix[0].length; i++)
+    {
+        if (matrix[idx][i] == '1')
+            height[i] += 1;
+        else
+            height[i] = 0;
+    }
+}
+
+public int largestInLine(int[] height)
+{
+    if (height == null || height.length < 1)
+        return 0;
+    int[] stack = new int[height.length + 1];
+    int len = 0, max = 0;
+    for (int i = 0; i <= height.length; i++)
+    {
+        int h = (i == height.length) ? 0 : height[i];
+        while (len != 0 && (i == height.length || height[stack[len - 1]] > h))
+        {
+            if (len == 1)
+                max = Math.max(height[stack[--len]] * i, max);
+            else
+                max = Math.max(height[stack[--len]] * (i - stack[len - 1] - 1), max);
+        }
+        stack[len++] = i;
+    }
+    return max;
+}
 }
