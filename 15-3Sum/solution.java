cde5e35c894c641a9f17388de03b38e766a9e22f
jiao-lc <939268350@qq.com>
2016-06-14 21:02:54 +0800

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -4,27 +4,19 @@ public class Solution {
         if(nums.length == 0)    return res;
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1]))
-                helper(res, nums, nums[i], i + 1, nums.length - 1);
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int low = i + 1, high = nums.length - 1;
+                while(low < high) {
+                    if (nums[low] + nums[high] + nums[i] == 0) {
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while (low < high && nums[low] == nums[low+1]) low++;
+                        while (low < high && nums[high] == nums[high-1]) high--;
+                        low++; high--;
+                    } else if (nums[low] + nums[high] + nums[i] < 0) low++;
+                    else high--;
+                }
+            }
         }
         return res;
-    }
-    
-    public void helper(List<List<Integer>> res, int[] nums, int target, int start, int end) {
-        if(start >= end)    return;
-        if(nums[start] + nums[end] + target == 0) {
-            List<Integer> ans = new ArrayList<Integer>();
-            ans.add(target);
-            ans.add(nums[start]);
-            ans.add(nums[end]);
-            res.add(ans);
-            helper(res, nums, target, start + 1, end - 1);
-        }
-        if(nums[start] + nums[end] + target > 0) {
-            helper(res, nums, target, start, end - 1);
-        }
-        if(nums[start] + nums[end] + target < 0) {
-            helper(res, nums, target, start + 1, end);
-        }
     }
 }
