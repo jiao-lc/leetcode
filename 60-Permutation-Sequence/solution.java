8f48c63fc82ad7503edfcd5862a3700b7f62086f
jiao-lc <939268350@qq.com>
2016-06-15 22:23:55 +0800

    [60. Permutation Sequence][Time Limit Exceeded]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,21 +1,17 @@
 public class Solution {
     public String getPermutation(int n, int k) {
-        if(n <= 0)  return "";
         k--;
-        StringBuilder res = new StringBuilder();
-        List<Integer> nums = new ArrayList<Integer>();
-        int factorial = 1;
-        for(int i = 2; i < n; i++)  factorial *= i;
-        for(int i = 1; i <= n; i++) nums.add(i);
-        int round = n - 1;
-        while(round >= 0) {
-            int index = k / factorial;
-            k %= factorial;
-            res.append(nums.get(index));
-            nums.remove(index);
-            if(round > 0)   factorial /= round;
-            round--;
+        int fact = 1;
+        char[] result = new char[n];
+        result[n - 1] = '1';
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+            result[n - i] = (char) (k % fact * i / fact + '1');
+            for (int j = n - i + 1; j < n; j++) {
+                result[j] += result[j] >= result[n - i] ? 1 : 0;
+            }
+            k -= k % fact;
         }
-        return res.toString();
+        return new String(result);
     }
 }
