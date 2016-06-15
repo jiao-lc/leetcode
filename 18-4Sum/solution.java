da0d97587bf871d68a78fdee73fa1bb38f08dc06
jiao-lc <939268350@qq.com>
2016-06-15 10:56:19 +0800

    [18. 4Sum][Time Limit Exceeded]committed by LeetCode Extension

--- 18-4Sum/solution.java
+++ 18-4Sum/solution.java
@@ -1,39 +1,41 @@
 public class Solution {
-public List<List<Integer>> fourSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    if(nums == null || nums.length < 4) return list;
-    Arrays.sort(nums);
-    int len = nums.length;
-
-    // anchor the first element
-    for(int i=0; i<=len-4; i++) {
-        if(i != 0 && nums[i] == nums[i-1]) continue;                                // avoid duplicate
-        if(nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;             // speed up
-        if(nums[i] + nums[len-3] + nums[len-2] + nums[len-1] < target) continue;    // speed up
-
-        // 3-sum
-        for(int j=i+1; j<=len-3; j++) {
-            if(j != i+1 && nums[j] == nums[j-1]) continue;                          // avoid duplicate
-            if(nums[i] + nums[j] + nums[i+1] + nums[j+2] > target) break;           // speed up
-            if(nums[i] + nums[j] + nums[len-2] + nums[len-1] < target) continue;    // speed up
-
-            // two pointer
-            int left = j+1, right = len-1, tmpTarget = target - nums[i] - nums[j];
-            while(left < right) {
-                if(nums[left] + nums[right] == tmpTarget) {
-                    list.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right])));
-                    while(left < right && nums[left] == nums[left+1]) left++;       // avoid duplicate
-                    while(left < right && nums[right] == nums[right-1]) right--;    // avoid duplicate
-                    left++;
-                    right--;
-                } else if(nums[left] + nums[right] < tmpTarget) {
-                    left++;
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int len = nums.length;
+        if(len == 0) return new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashSet<List<Integer>> res = new HashSet<>();
+        for(int i = 0; i < len - 1; i++) {
+            for(int j = i + 1; j < len; j++) {
+                int twoSum = nums[i] + nums[j];
+                if(map.containsKey(target - twoSum)) {
+                    List<Integer> ls = map.get(target - twoSum);
+                    for(Integer k : ls) {
+                        int m1 = Math.min(k / nums.length, i);  // m1 will always be the smallest index
+                        int m2 = Math.min(k % nums.length, j);  // m2 will be one of the middle two indices
+                        int m3 = Math.max(k / nums.length, i);  // m3 will be one of the middle two indices
+                        int m4 = Math.max(k % nums.length, j);  // m4 will always be the largest index
+                        
+                        if(m1 == m3 || m1 == m4 || m2 == m3 || m2 == m4)    continue;
+                        if(m2 > m3) {
+                            res.add(Arrays.asList(nums[m1], nums[m3], nums[m2], nums[m4]));
+                        } else {
+                            res.add(Arrays.asList(nums[m1], nums[m2], nums[m3], nums[m4]));
+                        }
+                    }
+                }
+                if(!map.containsKey(twoSum)) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(i * len + j);
+                    map.put(twoSum, list);
                 } else {
-                    right--;
+                    List<Integer> list = map.get(twoSum);
+                    list.add(i * len + j);
+                    map.put(twoSum, list);
                 }
             }
         }
+        
+        return new ArrayList<List<Integer>>(res);
     }
-    return list;
-}
 }
