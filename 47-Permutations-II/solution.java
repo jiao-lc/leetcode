59c96cd11ac38a0a4e92f68d714b75c89e6937ec
jiao-lc <939268350@qq.com>
2016-06-15 16:47:56 +0800

    [47. Permutations II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 47-Permutations-II/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums.length == 0) return res;
+        boolean[] visited = new boolean[nums.length];
+        helper(res, new ArrayList<Integer>(), nums, visited);
+        return res;
+    }
+    public void helper(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] visited) {
+        if(list.size() == nums.length) {
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i]) continue;
+            if(i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])  continue;
+            visited[i] = true;
+            list.add(nums[i]);
+            helper(res, list, nums, visited);
+            visited[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+}
