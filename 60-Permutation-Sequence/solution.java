d690cf01d8053b34309b64bad80356492748bb65
jiao-lc <939268350@qq.com>
2016-06-15 22:22:14 +0800

    [60. Permutation Sequence][Time Limit Exceeded]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,7 +1,6 @@
 public class Solution {
     public String getPermutation(int n, int k) {
         if(n <= 0)  return "";
-        if(n == 8 && k == 8590) return "26847351";
         k--;
         StringBuilder res = new StringBuilder();
         List<Integer> nums = new ArrayList<Integer>();
