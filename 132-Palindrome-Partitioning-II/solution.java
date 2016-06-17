8c7855f4f7fd36ec90ec4c90266e74c352818d37
jiao-lc <939268350@qq.com>
2016-06-17 15:54:21 +0800

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -3,20 +3,16 @@ public class Solution {
         char[] c = s.toCharArray();
         int len = c.length;
         if(len == 0)    return 0;
-        boolean[][] p = new boolean[len][len];
+        int[] step = new int[len + 1];
+        for (int i = 0; i <= len; i++) step[i] = i-1;
         for(int i = 0; i < len; i++) {
-            for(int j = 0; j < len - i; j++) {
-                p[j][j + i] = c[j] == c[j + i] && (i <= 1 || p[j + 1][j + i - 1]);
+            for(int j = 0; i - j >= 0 && i + j < len && s.charAt(i - j) == s.charAt(i + j); j++) {
+                step[j + i + 1] = Math.min(step[j + i + 1], 1 + step[i - j]);
+            }
+            for(int j = 1; i - j + 1 >= 0 && i + j < len && s.charAt(i - j + 1) == s.charAt(i + j); j++) {
+                step[j + i + 1] = Math.min(step[j + i + 1], 1 + step[i - j + 1]);
             }
         }
-        int[] step = new int[len];
-        Arrays.fill(step, Integer.MAX_VALUE);
-        step[0] = 0;
-        for(int i = 1; i < len; i++) {
-            for(int j = 0; j <= i; j++) {
-                if(p[j][i]) step[i] = j > 0 ? Math.min(step[i], 1 + step[j - 1]) : 0;
-            }
-        }
-        return step[len - 1];
+        return step[len];
     }
 }
