dfd54bb6a01667b1674c93f4375170e672227385
jiao-lc <939268350@qq.com>
2016-06-17 17:14:01 +0800

    [307. Range Sum Query - Mutable][Time Limit Exceeded]committed by LeetCode Extension

--- 307-Range-Sum-Query---Mutable/solution.java
+++ 307-Range-Sum-Query---Mutable/solution.java
@@ -1,22 +1,67 @@
 public class NumArray {
-    public List<Integer> sum = new ArrayList<Integer>();
+    class TreeNode {
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+    }
+    TreeNode root = null;
+    public void init(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+        this.root = buildTree(0, nums.length-1, nums);
+    }
+    public TreeNode buildTree(int start, int end, int[] data) {
+        TreeNode t = new TreeNode();
+        t.start = start;
+        t.end = end;
+        if(start == end) {
+            t.sum = data[start];
+            return t;
+        }        
+        int mid = start + (end-start)/2;
+        t.left = buildTree(start, mid, data);
+        t.right = buildTree(mid+1, end, data);
+        t.sum = t.left.sum + t.right.sum;
+        return t;
+    }
     public NumArray(int[] nums) {
-        int c = 0;
-        for(int i : nums) {
-            c += i;
-            sum.add(c);
-        }
+         init(nums);
     }
 
+    public void updateTree(TreeNode node, int index, int val) {
+        if(node == null) return;
+        if(node.start == node.end) {
+            node.sum = val;
+            return;
+        }
+        int mid = node.start + (node.end - node.start)/2;
+        if(index <= mid) {
+            updateTree(node.left, index, val);
+        }else {
+            updateTree(node.right, index, val);
+        }
+        node.sum = node.left.sum + node.right.sum;
+    }
+    
     void update(int i, int val) {
-        int diff = val - (i == 0 ? sum.get(0) : sum.get(i) - sum.get(i - 1));
-        for(int j = i; j < sum.size(); j++) {
-            sum.set(j, sum.get(j) + diff);
-        }
+        updateTree(this.root, i, val);
     }
 
+    public int queryTree(TreeNode node, int left, int right) {
+        if(node == null) return 0;
+        if(node.start == left && node.end == right) return node.sum;
+        int mid = node.start + (node.end - node.start)/2;
+        if(mid >= right) {
+            return queryTree(node.left, left, right);
+        }else if(mid < left) {
+            return queryTree(node.right, left, right);
+        }else {
+            return queryTree(node.left, left, mid) + queryTree(node.right, mid+1, right);
+        }
+    }
     public int sumRange(int i, int j) {
-        return i == 0 ? sum.get(j) : sum.get(j) - sum.get(i - 1);
+        return queryTree(root, i, j);
     }
 }
 
