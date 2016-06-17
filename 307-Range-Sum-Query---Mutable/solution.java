0f45bcd0034253f526194be1a419337310ec05c1
jiao-lc <939268350@qq.com>
2016-06-17 17:23:10 +0800

    [307. Range Sum Query - Mutable][Accepted]committed by LeetCode Extension

--- 307-Range-Sum-Query---Mutable/solution.java
+++ 307-Range-Sum-Query---Mutable/solution.java
@@ -1,79 +1,38 @@
 public class NumArray {
-    private class SegmentTreeNode {
-        public int start;
-        public int end;
-        public int sum;
-        public SegmentTreeNode left, right;
-        public SegmentTreeNode(int start, int end) {
-            this.start = start;
-            this.end = end;
-            this.sum = 0;
-        }
-    }
-    
-    private SegmentTreeNode root;
+    private int BIT[];               // Binary Indexed Tree = Fenwick Tree
+    private int[] nums;
     
     public NumArray(int[] nums) {
-        this.root = buildTree(nums, 0, nums.length - 1);    
+        BIT = new int[nums.length + 1];
+        for(int i = 0; i < nums.length; i++) {
+            init(i + 1, nums[i]);
+        }
+        this.nums = nums;
     }
 
-    public void update(int i, int val) {
-        update(root, i, val);
+    private void init(int i, int val) {
+        while(i < BIT.length) {
+            BIT[i] += val;
+            i = i + (i & -i);
+        }
     }
     
-    private void update(SegmentTreeNode node, int position, int val) {
-        if(node.start == position && node.end == position) {
-            node.sum = val;
-            return;
-        }
-        int mid = node.start + (node.end - node.start) / 2;
-        if(position <= mid) {
-            update(node.left, position, val);
-        } else {
-            update(node.right, position, val);
-        }
-        node.sum = node.left.sum + node.right.sum;
+    public void update(int i, int val) {
+        int delta = val - nums[i];
+        nums[i] = val;
+        init(i + 1, delta);
     }
 
     public int sumRange(int i, int j) {
-        return sumRange(root, i, j);
+        return getSum(j + 1) - getSum(i);
     }
     
-    private int sumRange(SegmentTreeNode node, int lo, int hi) {
-        if(node.start == lo && node.end == hi) {
-            return node.sum;
+    private int getSum(int i) {
+        int sum = 0;
+        while(i > 0) {
+            sum += BIT[i];
+            i = i - (i & -i);
         }
-        int mid = node.start + (node.end - node.start) / 2;
-        if(hi <= mid) {
-            return sumRange(node.left, lo, hi);
-        } else if (lo > mid) {
-            return sumRange(node.right, lo, hi);
-        } else {
-            return sumRange(node.left, lo, mid) + sumRange(node.right, mid + 1, hi);
-        }
-    }
-    
-    private SegmentTreeNode buildTree(int[] nums, int lo, int hi) {
-        if(lo > hi) {
-            return null;
-        } else {
-            SegmentTreeNode node = new SegmentTreeNode(lo, hi);
-            if(lo == hi) {
-                node.sum = nums[lo];
-            } else {
-                int mid = lo + (hi - lo) / 2;
-                node.left = buildTree(nums, lo, mid);
-                node.right = buildTree(nums, mid + 1, hi);
-                node.sum = node.left.sum + node.right.sum;
-            }
-            return node;
-        }
+        return sum;
     }
 }
-
-
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray = new NumArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.update(1, 10);
-// numArray.sumRange(1, 2);
