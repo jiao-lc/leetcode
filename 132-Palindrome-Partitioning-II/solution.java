6700205766441dd8ffa4bfd2fdc7f2fe5e98110e
jiao-lc <939268350@qq.com>
2016-06-17 16:01:14 +0800

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -1,18 +1,20 @@
 public class Solution {
-    public int minCut(String s) {
-        char[] c = s.toCharArray();
-        int len = c.length;
-        if(len == 0)    return 0;
-        int[] step = new int[len + 1];
-        for (int i = 0; i <= len; i++) step[i] = i-1;
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; i - j >= 0 && i + j < len && s.charAt(i - j) == s.charAt(i + j); j++) {
-                step[j + i + 1] = Math.min(step[j + i + 1], 1 + step[i - j]);
-            }
-            for(int j = 1; i - j + 1 >= 0 && i + j < len && s.charAt(i - j + 1) == s.charAt(i + j); j++) {
-                step[j + i + 1] = Math.min(step[j + i + 1], 1 + step[i - j + 1]);
+public int minCut(String s) {
+    char[] c = s.toCharArray();
+    int n = c.length;
+    int[] cut = new int[n];
+    boolean[][] pal = new boolean[n][n];
+
+    for(int i = 0; i < n; i++) {
+        int min = i;
+        for(int j = 0; j <= i; j++) {
+            if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                pal[j][i] = true;  
+                min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
             }
         }
-        return step[len];
+        cut[i] = min;
     }
+    return cut[n - 1];
+}
 }
