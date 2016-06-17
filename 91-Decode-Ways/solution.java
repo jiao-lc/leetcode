cd54bb6547f26c7952262e351573064f6410213a
jiao-lc <939268350@qq.com>
2016-06-17 16:34:08 +0800

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -9,12 +9,11 @@ public class Solution {
             if(i == 1)  cnt[i] = 1;
             else {
                 int c = s.charAt(i - 1) - '0';
-                if(s.charAt(i - 2) == '1' && c != 0 || s.charAt(i - 2) == '2' && c <= 6 && c != 0)  cnt[i] = cnt[i - 1] + cnt[i - 2];
-                else if (s.charAt(i - 1) == '0' && (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2')) cnt[i] = cnt[i - 2];
-                else if (c != 0)  cnt[i] = cnt[i - 1];
+                if(c == 0 && (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2')) cnt[i] = cnt[i - 2];
+                else if(s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2' && c <= 6)  cnt[i] = cnt[i - 1] + cnt[i - 2];
+                else if(c != 0) cnt[i] = cnt[i - 1];
                 else return 0;
             }
-            System.out.println(cnt[i]);
         }
         return cnt[len];
     }
