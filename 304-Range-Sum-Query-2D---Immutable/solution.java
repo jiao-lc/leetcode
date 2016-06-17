952ec4b1f8d8165efa28fcd47b21857dda3fa9d8
jiao-lc <939268350@qq.com>
2016-06-17 20:01:27 +0800

    [304. Range Sum Query 2D - Immutable][Accepted]committed by LeetCode Extension

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -1,26 +1,31 @@
 public class NumMatrix {
-    public int[][] sum;
+    public int[][] matrix;
     public NumMatrix(int[][] matrix) {
-        if(matrix.length == 0)  return;
+        if(matrix == null || matrix.length == 0)  return;
         int m = matrix.length, n = matrix[0].length;
-        int[][] sum = new int[m][n];
-        this.sum = sum;
-        for(int i = 0; i < m; i++) {
-            int c = 0;
+        this.matrix = matrix;
+        for(int i = 1; i < n; i++) {
+            matrix[0][i] += matrix[0][i - 1];
+        }
+        for(int i = 1; i < m; i++) {
+            int pre = 0;
             for(int j = 0; j < n; j++) {
-                c += matrix[i][j];
-                sum[i][j] = c;
+                int tmp = pre + matrix[i][j];
+                matrix[i][j] += pre + matrix[i - 1][j];
+                pre = tmp;
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = 0;
-        for(int i = row1; i <= row2; i++) {
-            ans += col1 == 0 ? sum[i][col2] : sum[i][col2] - sum[i][col1 - 1];
-        }
-        return ans;
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        if (row1 == 0 && col1 == 0) return matrix[row2][col2];
+        if (row1 == 0) return matrix[row2][col2] - matrix[row2][col1 - 1];
+        if (col1 == 0) return matrix[row2][col2] - matrix[row1 - 1][col2];
+        return matrix[row2][col2] + matrix[row1 - 1][col1 - 1] - matrix[row2][col1 - 1] - matrix[row1 - 1][col2];
     }
+
+    
 }
 
 
