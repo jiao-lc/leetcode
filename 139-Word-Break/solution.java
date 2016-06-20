0ac84ac9eb9e756dd6a49642761a569ef4ea5cfc
jiao-lc <939268350@qq.com>
2016-06-20 15:20:38 +0800

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -7,9 +7,7 @@ public class Solution {
                 map.put(str.charAt(0), new ArrayList<String>());
             }
             List<String> tmp = map.get(str.charAt(0));
-            int i = 0;
-            while(i < tmp.size() && str.compareTo(tmp.get(i)) > 0) i++;
-            tmp.add(i, str);
+            tmp.add(str);
         }
         int len = s.length();
         boolean[] res = new boolean[len + 1];
@@ -18,14 +16,12 @@ public class Solution {
             if(map.containsKey(s.charAt(i - 1))) {
                 List<String> array = map.get(s.charAt(i - 1));
                 for(String str : array) {
-                    int l = str.length();   //System.out.println(s.substring(i - 1, i + l - 1));
-                    if(s.substring(i - 1, i + l - 1).equals(str))   res[i + l - 1] = res[i - 1];
-                    if(i < len && s.substring(i - 1, i + 1).compareTo(str) > 0)  break;
+                    int l = str.length();   //System.out.println(str);
+                    if(i + l - 1 <= len && s.substring(i - 1, i + l - 1).equals(str))  res[i + l - 1] |= res[i - 1]; 
+                    //if(i < len && str.length() > 1 && s.charAt(i) > str.charAt(1))  break;
                 }
             }
-            //System.out.println(res[i]);
         }
-        
         return res[len];
     }
 }
