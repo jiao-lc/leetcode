32c5e508baa8785a7404409311049a8399ca3147
jiao-lc <939268350@qq.com>
2016-06-20 16:46:31 +0800

    [152. Maximum Product Subarray][Time Limit Exceeded]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -1,28 +1,19 @@
 public class Solution {
-    public int maxProduct(int[] nums) {
-        if(nums==null)return 0;
-           return subMaxPro(nums, 0, nums.length-1);
-    }
-     public int subMaxPro(int[] nums,int start,int end){
-         if(start>end)return 0;
-         if(start==end)return nums[start];
-         int p1=1,p2=1,p3=0;
-         int count=0;
-         for(int i = start;i<=end;i++){
-             if(nums[i]==0){
-                 if(count%2==0){
-                     return Math.max(i==start?0:p1,subMaxPro(nums, i+1, end));
-                 }
-                 return Math.max(i-start==1?0:Math.max(p1/p2,p3),subMaxPro(nums, i+1, end));
-             }else{
-                 p1*=nums[i];
-                 if(nums[i]<0){
-                     if(count==0)p2=p1;
-                     count++;
-                 }
-                     if(count%2==0)p3=p1;
-             }
-         }
-         return Math.max(p1/p2,p3);
-     }
+public int maxProduct(int[] A) {
+   int min;
+   int res = A[0], max = min = A[0]; // max, min means max and min product among the subarrays whose last element is A[i].
+   for (int i = 1; i < A.length; i++) {
+       if (A[i] > 0) {
+           max = Math.max(max * A[i], A[i]);
+           min = Math.min(min * A[i], A[i]);               
+       }
+       else {
+           int lastMax = max;
+           max = Math.max(min * A[i], A[i]);
+           min = Math.min(lastMax * A[i], A[i]);                           
+       }
+       res = Math.max(res, max);
+   }
+   return res;
+}
 }
