d4e90d90bad5dffa25843ae173d0ed652ff7d424
jiao-lc <939268350@qq.com>
2016-06-20 16:43:42 +0800

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -1,13 +1,28 @@
 public class Solution {
     public int maxProduct(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        int max = nums[0], min = nums[0], ans = nums[0];
-        for(int i = 1; i < nums.length; i++) {
-            int tmp = min * nums[i];
-            min = Math.min(nums[i], Math.min(tmp, max * nums[i]));
-            max = Math.max(nums[i], Math.max(tmp, max * nums[i]));
-            ans = Math.max(ans, max);
-        }
-        return ans;
+        if(nums==null)return 0;
+           return subMaxPro(nums, 0, nums.length-1);
     }
+     public int subMaxPro(int[] nums,int start,int end){
+         if(start>end)return 0;
+         if(start==end)return nums[start];
+         int p1=1,p2=1,p3=0;
+         int count=0;
+         for(int i = start;i<=end;i++){
+             if(nums[i]==0){
+                 if(count%2==0){
+                     return Math.max(i==start?0:p1,subMaxPro(nums, i+1, end));
+                 }
+                 return Math.max(i-start==1?0:Math.max(p1/p2,p3),subMaxPro(nums, i+1, end));
+             }else{
+                 p1*=nums[i];
+                 if(nums[i]<0){
+                     if(count==0)p2=p1;
+                     count++;
+                 }
+                     if(count%2==0)p3=p1;
+             }
+         }
+         return Math.max(p1/p2,p3);
+     }
 }
