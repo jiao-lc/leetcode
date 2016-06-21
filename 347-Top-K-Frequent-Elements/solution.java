78b9848d18c85484dccf055ca0d64119ba47231b
jiao-lc <939268350@qq.com>
2016-06-21 15:45:18 +0800

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -5,19 +5,16 @@ public class Solution {
             map.put(n, map.getOrDefault(n,0)+1);
         }
 
-        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
-        for(int num : map.keySet()){
-           int freq = map.get(num);
-           if(!freqMap.containsKey(freq)){
-               freqMap.put(freq, new LinkedList<>());
-           }
-           freqMap.get(freq).add(num);
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = 
+                         new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            maxHeap.add(entry);
         }
 
         List<Integer> res = new ArrayList<>();
         while(res.size()<k){
-            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
-            res.addAll(entry.getValue());
+            Map.Entry<Integer, Integer> entry = maxHeap.poll();
+            res.add(entry.getKey());
         }
         return res;
     }
