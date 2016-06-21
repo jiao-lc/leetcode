8518ac825c74103e6ad851e3945309ea34c32a52
jiao-lc <939268350@qq.com>
2016-06-21 16:17:40 +0800

    [147. Insertion Sort List][Accepted]committed by LeetCode Extension

---
+++ 147-Insertion-Sort-List/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null)   return head;
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while(head != null) {
+            while(p.next != null && p.next.val < head.val) p = p.next;
+            ListNode tmp = head.next;
+            head.next = p.next;
+            p.next = head;
+            head = tmp;
+            p = dummy;
+        }
+        return dummy.next;
+    }
+}
