cf342a6a27de4be0687cc1728be731b9e949c6e7
jiao-lc <939268350@qq.com>
2016-06-21 16:06:39 +0800

    [148. Sort List][Runtime Error]committed by LeetCode Extension

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -9,16 +9,39 @@
 public class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null)   return head;
+        return sort(head);
+    }
+    
+    public ListNode sort(ListNode head) {
+        if(head == null || head.next == null)   return head;
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null;
+        head = sort(head);
+        head2 = sort(head2);
+        return merge(head, head2);
+    }
+    
+    public ListNode merge(ListNode h1, ListNode h2) {
         ListNode dummy = new ListNode(0);
         ListNode p = dummy;
-        while(head != null) {
-            while(p.next != null && p.next.val < head.val) p = p.next;
-            ListNode tmp = head.next;
-            head.next = p.next;
-            p.next = head;
-            head = tmp;
-            p = dummy;
+        while(h1 != null && h2 != null) {
+            if(h1.val < h2.val) {
+                p.next = h1;
+                p = p.next;
+                h1 = h1.next;
+            } else {
+                p.next = h2;
+                p = p.next;
+                h2 = h2.next;
+            }
         }
+        if(h1 == null) p.next = h2;
+        else p.next = h1;
         return dummy.next;
     }
 }
