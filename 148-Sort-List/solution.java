3b4d16a53243b5711c89cb715140d0d0b9104d93
jiao-lc <939268350@qq.com>
2016-06-21 16:08:38 +0800

    [148. Sort List][Accepted]committed by LeetCode Extension

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -14,7 +14,7 @@ public class Solution {
     
     public ListNode sort(ListNode head) {
         if(head == null || head.next == null)   return head;
-        ListNode fast = head, slow = head;
+        ListNode fast = head.next, slow = head;
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
