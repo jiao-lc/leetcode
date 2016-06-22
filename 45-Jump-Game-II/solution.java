7dafb4bf045443706c9dfb5b912479d429ada642
jiao-lc <939268350@qq.com>
2016-06-22 21:31:05 +0800

    [45. Jump Game II][Runtime Error]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int jump(int[] nums) {
+        int len = nums.length;
+        if(len == 0)    return 0;
+        int[] step = new int[len];
+        Arrays.fill(step, Integer.MAX_VALUE);
+        step[0] = 0;
+        for(int i = 0; i < len; i++) {
+            for(int j = 1; j <= nums[i]; j++)
+                step[i + j] = Math.min(step[i + j], step[i] + 1);
+        }
+        return step[len - 1];
+    }
+}
