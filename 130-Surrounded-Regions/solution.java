a48ab00fafd783d0e8d5517bdab1ba10a8de597a
jiao-lc <939268350@qq.com>
2016-06-24 10:52:23 +0800

    [130. Surrounded Regions][Runtime Error]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -18,7 +18,7 @@ public class Solution {
         }
     }
     public void dfs(char[][] board, int i, int j) {
-        if(i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1 || board[i][j] == 'X')   return;
+        if(i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1 || board[i][j] == 'X' || board[i][j] == 'E')   return;
         if(board[i][j] == 'O')  board[i][j] = 'E';
         dfs(board, i, j + 1);
         dfs(board, i, j - 1);
