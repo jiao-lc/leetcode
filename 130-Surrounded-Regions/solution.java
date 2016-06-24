7c745a5095cc1ed35835a74ae4de3c2974da48bb
jiao-lc <939268350@qq.com>
2016-06-24 11:00:20 +0800

    [130. Surrounded Regions][Accepted]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -1,28 +1,64 @@
 public class Solution {
-    public void solve(char[][] board) {
-        if(board.length == 0 || board[0].length == 0)   return;
-        int m = board.length, n = board[0].length;
-        for(int j = 0; j < n; j++) {
-            if(board[0][j] == 'O')  dfs(board, 0, j);
-            if(board[m - 1][j] == 'O')  dfs(board, 0, j);
-        }
-        for(int i = 1; i < m - 1; i++) {
-            if(board[i][0] == 'O')  dfs(board, i, 0);
-            if(board[i][n - 1] == 'O')  dfs(board, i, 0);
-        }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'E')  board[i][j] = 'O';
-                else board[i][j] = 'X';
-            }
-        }
-    }
-    public void dfs(char[][] board, int i, int j) {
-        if(i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1 || board[i][j] == 'X' || board[i][j] == 'E')   return;
-        if(board[i][j] == 'O')  board[i][j] = 'E';
-        dfs(board, i, j + 1);
-        dfs(board, i, j - 1);
-        dfs(board, i + 1, j);
-        dfs(board, i - 1, j);
-    }
+	// use a queue to do BFS
+	private Queue<Integer> queue = new LinkedList<Integer>();
+	public void solve(char[][] board) {
+		if (board == null || board.length == 0)			return;
+		int m = board.length;
+		int n = board[0].length;
+		// merge O's on left & right boarder
+		for (int i = 0; i < m; i++) {
+			if (board[i][0] == 'O') {
+				bfs(board, i, 0);
+			}
+			if (board[i][n - 1] == 'O') {
+				bfs(board, i, n - 1);
+			}
+		}
+ 
+		// merge O's on top & bottom boarder
+		for (int j = 0; j < n; j++) {
+			if (board[0][j] == 'O') {
+				bfs(board, 0, j);
+			}
+			if (board[m - 1][j] == 'O') {
+				bfs(board, m - 1, j);
+			}
+		}
+ 
+		// process the board
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 'O') {
+					board[i][j] = 'X';
+				} else if (board[i][j] == '#') {
+					board[i][j] = 'O';
+				}
+			}
+		}
+	}
+	private void bfs(char[][] board, int i, int j) {
+		int n = board[0].length;
+		// fill current first and then its neighbors
+		fillCell(board, i, j);
+ 
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+			int x = cur / n;
+			int y = cur % n;
+			fillCell(board, x - 1, y);
+			fillCell(board, x + 1, y);
+			fillCell(board, x, y - 1);
+			fillCell(board, x, y + 1);
+		}
+	}
+ 
+	private void fillCell(char[][] board, int i, int j) {
+		int m = board.length;
+		int n = board[0].length;
+		if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O')
+			return;
+		// add current cell is queue & then process its neighbors in bfs
+		queue.offer(i * n + j);
+		board[i][j] = '#';
+	}
 }
