562686231688d7401d34f45108cc39dc81b4c505
jiao-lc <939268350@qq.com>
2016-06-24 10:25:06 +0800

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -2,8 +2,11 @@ public class Solution {
     public String reverseWords(String s) {
         if(s.length() == 0) return s;
         String[] arr = s.split(" ");
+        String t = "";
+        if(arr.length == 0) return t;
         String ans = arr[arr.length - 1];
         for(int i = arr.length - 2; i >= 0; i--) {
+            if(arr[i].length() == 0) continue;
             ans = ans + " " + arr[i];
         }
         return ans;
