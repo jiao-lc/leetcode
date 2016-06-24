199b380d2f477059757dfdaabfcafdd28c5b47d4
jiao-lc <939268350@qq.com>
2016-06-24 09:20:56 +0800

    [301. Remove Invalid Parentheses][Accepted]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.java
+++ 301-Remove-Invalid-Parentheses/solution.java
@@ -3,23 +3,23 @@ public class Solution {//BFS
         List<String> res = new ArrayList<String>();
         if(s.length() == 0) {res.add(""); return res;}
         Queue<String> queue = new LinkedList<String>();
-        Set<String> sub = new HashSet<String>();
+        Set<String> visited = new HashSet<String>();
         boolean found = false;
         queue.add(s);
         while(queue.peek() != null) {
             String tmp = queue.poll();
             //System.out.println(tmp);
-            if(valid(tmp))  {found = true; sub.add(tmp);}
+            if(valid(tmp))  {found = true; res.add(tmp);}
             if(found)   continue;
             for(int i = 0; i < tmp.length(); i++) {
-                if(tmp.charAt(i) != '(' && tmp.charAt(i) != ')' || tmp.length() == 1)    continue;
-                if(i == 0)  queue.add(tmp.substring(1));
-                else if(i == tmp.length() - 1) queue.add(tmp.substring(0, tmp.length() - 1));
-                else queue.add(tmp.substring(0, i) + tmp.substring(i + 1, tmp.length()));
+                if(tmp.charAt(i) != '(' && tmp.charAt(i) != ')') continue;  
+                String sub = tmp.substring(0,i) + tmp.substring(i+1); 
+                if(!visited.contains(sub)) {  
+                    queue.offer(sub);  
+                    visited.add(sub);  
+                }
             }
         }
-        res.addAll(sub);
-        if(res.size() == 0) res.add("");
         return res;
     }
     public boolean valid(String s) {
