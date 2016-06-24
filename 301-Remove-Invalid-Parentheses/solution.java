10dda8f1d26fe896fc07c4a3d1efa0f98ca12c03
jiao-lc <939268350@qq.com>
2016-06-24 09:45:54 +0800

    [301. Remove Invalid Parentheses][Accepted]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.java
+++ 301-Remove-Invalid-Parentheses/solution.java
@@ -1,34 +1,24 @@
-public class Solution {//BFS
-    public List<String> removeInvalidParentheses(String s) {
-        List<String> res = new ArrayList<String>();
-        if(s.length() == 0) {res.add(""); return res;}
-        Queue<String> queue = new LinkedList<String>();
-        Set<String> visited = new HashSet<String>();
-        boolean found = false;
-        queue.add(s);
-        while(queue.peek() != null) {
-            String tmp = queue.poll();
-            //System.out.println(tmp);
-            if(valid(tmp))  {found = true; res.add(tmp);}
-            if(found)   continue;
-            for(int i = 0; i < tmp.length(); i++) {
-                if(tmp.charAt(i) != '(' && tmp.charAt(i) != ')') continue;  
-                String sub = tmp.substring(0,i) + tmp.substring(i+1); 
-                if(!visited.contains(sub)) {  
-                    queue.offer(sub);  
-                    visited.add(sub);  
-                }
-            }
-        }
-        return res;
+public class Solution {
+public List<String> removeInvalidParentheses(String s) {
+    List<String> ans = new ArrayList<>();
+    remove(s, ans, 0, 0, new char[]{'(', ')'});
+    return ans;
+}
+
+public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+    for (int stack = 0, i = last_i; i < s.length(); ++i) {
+        if (s.charAt(i) == par[0]) stack++;
+        if (s.charAt(i) == par[1]) stack--;
+        if (stack >= 0) continue;
+        for (int j = last_j; j <= i; ++j)
+            if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+        return;
     }
-    public boolean valid(String s) {
-        int cnt = 0;
-        for(char c : s.toCharArray()) {
-            if(c == '(')    cnt++;
-            if(c == ')')    cnt--;
-            if(cnt < 0) return false;
-        }
-        return cnt == 0;
-    }
+    String reversed = new StringBuilder(s).reverse().toString();
+    if (par[0] == '(') // finished left to right
+        remove(reversed, ans, 0, 0, new char[]{')', '('});
+    else // finished right to left
+        ans.add(reversed);
+}
 }
