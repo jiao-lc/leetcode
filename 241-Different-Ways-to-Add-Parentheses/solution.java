3caaecff7c5e4f7018edee16d6cde866736bc0bc
jiao-lc <939268350@qq.com>
2016-06-27 15:53:04 +0800

    [241. Different Ways to Add Parentheses][Accepted]committed by LeetCode Extension

--- 241-Different-Ways-to-Add-Parentheses/solution.java
+++ 241-Different-Ways-to-Add-Parentheses/solution.java
@@ -1,32 +1,59 @@
 public class Solution {
     public List<Integer> diffWaysToCompute(String input) {
-        List<Integer> list=new ArrayList();
-        char[] c=input.toCharArray();
-        boolean sign=true;
-        for(int i=0;i<c.length;i++){
-            if(c[i]=='-'||c[i]=='+'||c[i]=='*'||c[i]=='/'){
-                List<Integer> l1 = diffWaysToCompute(input.substring(0,i));
-                List<Integer> l2 = diffWaysToCompute(input.substring(i+1,c.length));
-                for(int j=0;j<l1.size();j++){
-                    for(int k=0;k<l2.size();k++){
-                        switch(c[i]){
-                            case '-': list.add(l1.get(j)-l2.get(k)); break;
-                            case '+': list.add(l1.get(j)+l2.get(k)); break;
-                            case '*': list.add(l1.get(j)*l2.get(k)); break;
-                            case '/': list.add(l1.get(j)/l2.get(k)); break;
+        if (input.length() == 0) {
+            return new ArrayList<Integer>();
+        }
+        List<Integer> numbers = new ArrayList<Integer>();
+        List<Character> operators = new ArrayList<Character>();
+    
+        int num = 0;
+        for (char c : input.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+            } else {
+                numbers.add(num);
+                operators.add(c);
+                num = 0;
+            }
+        }
+        numbers.add(num); // add last num
+    
+        List<Integer>[][] dp = new List[input.length()][input.length()];
+        return diffWaysToCompute(numbers, operators, 0, numbers.size() - 1, dp);
+    }
+    
+    private List<Integer> diffWaysToCompute(List<Integer> numbers, List<Character> operators, int start, int end, List<Integer>[][] dp) {
+        if (dp[start][end] != null) {
+            return dp[start][end];
+        } else {
+            List<Integer> values = new ArrayList<Integer>();
+            if(start == end) {
+                values.add(numbers.get(start));
+            } else {
+                for (int i = start; i < end; i++) { // index of operator
+                    List<Integer> left = diffWaysToCompute(numbers, operators, start, i, dp);
+                    List<Integer> right = diffWaysToCompute(numbers, operators, i + 1, end, dp);
+                    char operator = operators.get(i);
+                        for (int a : left) {
+                            for (int b : right) {
+                                int value = compute(a, b, operator);
+                                values.add(value);
+                            }
                         }
                     }
                 }
-                sign=false;
-            }
+        dp[start][end] = values;
+        return values;
         }
-        if(sign){
-            int sum=0;
-            for(char c1:input.toCharArray()){
-                sum=sum*10+c1-'0';
-            }
-            list.add(sum);
+    }
+    private int compute(int a, int b, char operator) {
+        switch (operator) {
+            case '+' : 
+                return a + b;
+            case '-' :
+                return a - b;
+            default : // '*'
+                return a * b;
         }
-        return list;
     }
 }
