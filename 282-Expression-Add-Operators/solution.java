d407a3b8855ca4d18cda2e08c316a4211d3cb0d9
jiao-lc <939268350@qq.com>
2016-06-27 17:02:31 +0800

    [282. Expression Add Operators][Accepted]committed by LeetCode Extension

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,27 +1,31 @@
 public class Solution {
-    public List<String> addOperators(String num, int target) {
-        List<String> res = new ArrayList<String>();
-        helper(res, "", 0, num, target, 0, 0);
-        return res;
+public List<String> addOperators(String num, int target) {
+    List<String> res = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    dfs(res, sb, num, 0, target, 0, 0);
+    return res;
+
+}
+public void dfs(List<String> res, StringBuilder sb, String num, int pos, int target, long prev, long multi) { 
+    if(pos == num.length()) {
+        if(target == prev) res.add(sb.toString());
+        return;
     }
-    
-    public void helper(List<String> res, String path, int pos, String num, int target, long eval, long multi) {
-        if(pos == num.length()) {
-            if(eval == target) {
-                res.add(path);
-            }
-            return;
-        }
-        for(int i = pos; i < num.length(); i++) {
-            if(i != pos && num.charAt(pos) == '0')  break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
-            if(pos == 0) {
-                helper(res, path + cur, i + 1, num, target, cur, cur);
-            } else {
-                helper(res, path + "+" + cur, i + 1, num, target, eval + cur, cur);
-                helper(res, path + "-" + cur, i + 1, num, target, eval - cur, -cur);
-                helper(res, path + "*" + cur, i + 1, num, target, eval - multi + multi * cur, multi * cur);
-            }
+    for(int i = pos; i < num.length(); i++) {
+        if(num.charAt(pos) == '0' && i != pos) break;
+        long curr = Long.parseLong(num.substring(pos, i + 1));
+        int len = sb.length();
+        if(pos == 0) {
+            dfs(res, sb.append(curr), num, i + 1, target, curr, curr); 
+            sb.setLength(len);
+        } else {
+            dfs(res, sb.append("+").append(curr), num, i + 1, target, prev + curr, curr); 
+            sb.setLength(len);
+            dfs(res, sb.append("-").append(curr), num, i + 1, target, prev - curr, -curr); 
+            sb.setLength(len);
+            dfs(res, sb.append("*").append(curr), num, i + 1, target, prev - multi + multi * curr, multi * curr); 
+            sb.setLength(len);
         }
     }
+}
 }
