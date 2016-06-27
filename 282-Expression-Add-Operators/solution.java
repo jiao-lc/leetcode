19d3f676e33c902179ebcc430c2cfdeb0d0f1cba
jiao-lc <939268350@qq.com>
2016-06-27 17:03:33 +0800

    [282. Expression Add Operators][Accepted]committed by LeetCode Extension

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,31 +1,35 @@
 public class Solution {
-public List<String> addOperators(String num, int target) {
-    List<String> res = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
-    dfs(res, sb, num, 0, target, 0, 0);
-    return res;
-
-}
-public void dfs(List<String> res, StringBuilder sb, String num, int pos, int target, long prev, long multi) { 
-    if(pos == num.length()) {
-        if(target == prev) res.add(sb.toString());
+public void dfs(List<String> ret, char[] path, int len, long left, long cur, char[] digits, int pos, int target) {
+    if (pos == digits.length) {
+        if (left + cur == target) ret.add(new String(path, 0, len));
         return;
     }
-    for(int i = pos; i < num.length(); i++) {
-        if(num.charAt(pos) == '0' && i != pos) break;
-        long curr = Long.parseLong(num.substring(pos, i + 1));
-        int len = sb.length();
-        if(pos == 0) {
-            dfs(res, sb.append(curr), num, i + 1, target, curr, curr); 
-            sb.setLength(len);
-        } else {
-            dfs(res, sb.append("+").append(curr), num, i + 1, target, prev + curr, curr); 
-            sb.setLength(len);
-            dfs(res, sb.append("-").append(curr), num, i + 1, target, prev - curr, -curr); 
-            sb.setLength(len);
-            dfs(res, sb.append("*").append(curr), num, i + 1, target, prev - multi + multi * curr, multi * curr); 
-            sb.setLength(len);
-        }
+    long n = 0;
+    int j = len + 1;
+    for (int i = pos; i < digits.length; i++) {
+        n = n * 10 + digits[i] - '0';
+        path[j++] = digits[i];
+        path[len] = '+';
+        dfs(ret, path, j, left + cur, n, digits, i + 1, target);
+        path[len] = '-';
+        dfs(ret, path, j, left + cur, -n, digits, i + 1, target);
+        path[len] = '*';
+        dfs(ret, path, j, left, cur * n, digits, i + 1, target);
+        if (digits[pos] == '0') break; 
     }
+}
+public List<String> addOperators(String num, int target) {
+    List<String> ret = new LinkedList<>();
+    if (num.length() == 0) return ret;
+    char[] path = new char[num.length() * 2 - 1];
+    char[] digits = num.toCharArray();
+    long n = 0;
+    for (int i = 0; i < digits.length; i++) {
+        n = n * 10 + digits[i] - '0';
+        path[i] = digits[i];
+        dfs(ret, path, i + 1, 0, n, digits, i + 1, target);
+        if (n == 0) break;
+    }
+    return ret;
 }
 }
