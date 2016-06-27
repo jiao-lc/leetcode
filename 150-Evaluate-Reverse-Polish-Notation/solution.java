3675d724fa11cd43811d67bebca31528be72e69b
jiao-lc <939268350@qq.com>
2016-06-27 16:27:56 +0800

    [150. Evaluate Reverse Polish Notation][Runtime Error]committed by LeetCode Extension

---
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        if(tokens.length == 0)  return 0;
+        int[] num = new int[tokens.length];
+        int len = 0;
+        for(String str : tokens) {
+            if(Character.isDigit(str.charAt(0))) {
+                num[len++] = Integer.parseInt(str);
+            } else {
+                int a = num[--len];
+                int b = num[--len];
+                num[len++] = calculate(b, a, str);
+            }
+        }
+        return num[len - 1];
+    }
+    public int calculate(int a, int b, String str) {
+        if(str.equals("+")) return a + b;
+        if(str.equals("-")) return a - b;
+        if(str.equals("*")) return a * b;
+        return a / b;
+    }
+}
