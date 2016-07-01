f452ed761aefeb4ae27a60965c651fbb0eb0d698
jiao-lc <939268350@qq.com>
2016-07-01 19:13:44 +0800

    [315. Count of Smaller Numbers After Self][Accepted]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,53 +1,56 @@
 public class Solution {
-//Trying to use Binary Indexed Tree to solve this problem
-//Time complexity is O(Nlog(N)) which is better than Binary Search Tree solution
-//Need to build an array with the lenght of (Max-Min), which performs worth than BST solution
-public List<Integer> countSmaller(int[] nums) {
-    List<Integer> result = new ArrayList<Integer>();
-    if(nums.length==0){
-        return result;
-    }
-    else{
-        //Firstly, we make all the numbers in the array to be non-negative by num[i] = num[i]-min
-        int min = Integer.MAX_VALUE;
-        for(int x : nums){
-            min = Math.min(x,min);
+    //merge sort
+    public int[] count;
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        count = new int[nums.length];
+        int[] indx = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            indx[i] = i;
         }
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i<nums.length; i++){
-            nums[i] -= min;
-            max = Math.max(nums[i],max);
+        mergeSort(nums, indx, 0, nums.length - 1);
+        for(int i = 0; i < nums.length; i++) {
+            res.add(count[i]);
         }
-        //Then build the array from 0 to max to realize the Binary Indexed Tree
-        //We traverse from the rightmost side towards leftmost side
-        //When we reach a number num, array[num] update by 1
-        int[] BIT = new int[max+1];
-        for(int i = nums.length-1; i>=0; i--){
-            result.add(getSum( BIT, nums[i]-1));
-            update( BIT, nums[i]);
+        return res;
+    }
+    
+    public void mergeSort(int[] nums, int[] indx, int start, int end) {
+        if(start >= end) return;
+        int mid = (start + end) / 2;
+        mergeSort(nums, indx, start, mid);
+        mergeSort(nums, indx, mid + 1, end);
+        merge(nums, indx, start, end);
+    }
+    public void merge(int[] nums, int[] indx, int start, int end) {
+        int mid = (start + end) / 2;
+        int left_index = start;
+        int right_index = mid+1;
+        int rightcount = 0;     
+        int[] new_indexes = new int[end - start + 1];
+        int sort_index = 0;
+        while(left_index <= mid && right_index <= end) {
+            if(nums[indx[left_index]] <= nums[indx[right_index]]) {
+                new_indexes[sort_index] = indx[left_index];
+                count[indx[left_index]] += rightcount;
+                left_index++;
+            } else {
+                new_indexes[sort_index] = indx[right_index];
+                rightcount++;
+                right_index++;
+            }
+            sort_index++;
         }
-        //Then we need to reverse the result List cause it was built in reversed order
-        Collections.reverse(result);
-        return result;
+        while(left_index <= mid) {
+            new_indexes[sort_index++] = indx[left_index];
+            count[indx[left_index]] += rightcount;
+            left_index++;
+        }
+        while(right_index <= end) {
+            new_indexes[sort_index++] = indx[right_index++];
+        }
+        for(int i = start; i <= end; i++){
+            indx[i] = new_indexes[i - start];
+        }
     }
-}
-private void update( int[] BIT, int val){
-    int fakeIndex = val+1;
-    while(fakeIndex<=BIT.length){
-        BIT[fakeIndex-1] += 1;
-        fakeIndex += fakeIndex&(-fakeIndex);
-    }
-}
-private int getSum(int[] BIT, int val){
-    if(val < 0){
-        return 0;
-    }
-    int sum = 0;
-    int fakeIndex = val+1;
-    while(fakeIndex > 0){
-        sum += BIT[fakeIndex-1];
-        fakeIndex -= fakeIndex&(-fakeIndex);
-    }
-    return sum;
-}
 }
