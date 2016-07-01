091c6a9890bda46ddba6f0765da1abb730493b7d
jiao-lc <939268350@qq.com>
2016-07-01 19:59:53 +0800

    [315. Count of Smaller Numbers After Self][Accepted]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -15,13 +15,12 @@ public List<Integer> countSmaller(int[] nums) {
 
 private void countSmallerSub(int[] nums, List<Integer> index, int mask, List<Integer> res) {
     if (mask != 0 && index.size() > 1) {
-        List<Integer> highGroup = new ArrayList<>(index.size());
         List<Integer> lowGroup = new ArrayList<>(index.size());
+        List<Integer> highGroup = new ArrayList<>(index.size());
 
-        int highBit = (mask < 0 ? 0 : mask);
-
+        int high = mask < 0 ? 0 : mask;
         for (int i = 0; i < index.size(); i++) {
-            if ((nums[index.get(i)] & mask) == highBit) {
+            if ((nums[index.get(i)] & mask) == high) {
                 res.set(index.get(i), res.get(index.get(i)) + lowGroup.size());
                 highGroup.add(index.get(i));
             } else {
