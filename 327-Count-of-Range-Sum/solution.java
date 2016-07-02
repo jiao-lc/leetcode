782046ece111a582e4fe2d8dd01f09829180318d
jiao-lc <939268350@qq.com>
2016-07-02 12:26:31 +0800

    [327. Count of Range Sum][Accepted]committed by LeetCode Extension

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -1,60 +1,83 @@
 public class Solution {
-public int countRangeSum(int[] nums, int lower, int upper) {
-    long[] sum = new long[nums.length + 1];
-    long[] cand = new long[3 * sum.length + 1];
-    int index = 0;
-    cand[index++] = sum[0];
-    cand[index++] = lower + sum[0] - 1;
-    cand[index++] = upper + sum[0];
-
-    for (int i = 1; i < sum.length; i++) {
-        sum[i] = sum[i - 1] + nums[i - 1];
-        cand[index++] = sum[i];
-        cand[index++] = lower + sum[i] - 1;
-        cand[index++] = upper + sum[i];
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int res = 0;
+        long [] sum = new long[nums.length+1];
+        for(int i = 1; i<sum.length; i++){
+            sum[i] = sum[i-1] + nums[i-1];
+        }
+        
+        TreeNode root = new TreeNode(sum[0]);
+        for(int i = 1; i<sum.length; i++){
+            res += rangeSize(root, sum[i]-upper, sum[i]-lower);
+            insert(root, sum[i]);
+        }
+        return res;
     }
-
-    cand[index] = Long.MIN_VALUE; // avoid getting root of the binary indexed tree when doing binary search
-    Arrays.sort(cand);
-
-    int[] bit = new int[cand.length];
-
-    // build up the binary indexed tree with only elements from the prefix array "sum"
-    for (int i = 0; i < sum.length; i++) {
-        addValue(bit, Arrays.binarySearch(cand, sum[i]), 1);
+    
+    private TreeNode insert(TreeNode root, long val){
+        if(root == null){
+            return new TreeNode(val);
+        }
+        if(root.val == val){
+            root.count++;
+        }else if(root.val > val){
+            root.leftSize++;
+            root.left = insert(root.left, val);
+        }else if(root.val < val){
+            root.rightSize++;
+            root.right = insert(root.right, val);
+        }
+        return root;
     }
-
-    int count = 0;
-
-    for (int i = 1; i < sum.length; i++) {
-        // get rid of visited elements by adding -1 to the corresponding tree nodes
-        addValue(bit, Arrays.binarySearch(cand, sum[i - 1]), -1);
-
-        // add the total number of valid elements with upper bound (upper + sum[i - 1])
-        count += query(bit, Arrays.binarySearch(cand, upper + sum[i - 1]));
-
-        // minus the total number of valid elements with lower bound (lower + sum[i - 1] - 1)
-        count -= query(bit, Arrays.binarySearch(cand, lower + sum[i - 1] - 1));
+    
+    private int countSmaller(TreeNode root, long val){
+        if(root == null){
+            return 0;
+        }
+        if(root.val == val){
+            return root.leftSize;
+        }else if(root.val > val){
+            return countSmaller(root.left, val);
+        }else{
+            return root.leftSize + root.count + countSmaller(root.right, val);
+        }
     }
-
-    return count;
+    
+    private int countLarget(TreeNode root, long val){
+        if(root == null){
+            return 0;
+        }
+        if(root.val == val){
+            return root.rightSize;
+        }else if(root.val > val){
+            return countLarget(root.left, val) + root.count + root.rightSize;
+        }else{
+            return countLarget(root.right, val);
+        }
+    }
+    
+    private int rangeSize(TreeNode root, long lower, long upper){
+        int total = root.leftSize + root.count + root.rightSize;
+        int smaller = countSmaller(root, lower);
+        int larger = countLarget(root, upper);
+        return total - smaller - larger;
+    }
 }
 
-private void addValue(int[] bit, int index, int value) {
-    while (index < bit.length) {
-        bit[index] += value;
-        index += index & -index;
+class TreeNode{
+    long val;
+    int count;
+    int leftSize;
+    int rightSize;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(long val){
+        this.val = val;
+        this.count = 1;
+        this.leftSize = 0;
+        this.rightSize = 0;
     }
-}
-
-private int query(int[] bit, int index) {
-    int sum = 0;
-
-    while (index > 0) {
-        sum += bit[index];
-        index -= index & -index;
-    }
-
-    return sum;
-}
 }
