2f84887428b271e7c11e7ceba9a973f41a6b99ed
jiao-lc <939268350@qq.com>
2016-07-02 16:52:06 +0800

    [220. Contains Duplicate III][Runtime Error]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,13 +1,18 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if(nums.length == 0 || k <= 0)    return false;
-        TreeSet<Long> set = new TreeSet<>();
-        for(int i = 0; i <  nums.length; i++) {
-            Long h = set.ceiling((long)nums[i]);
-            Long l = set.floor((long)nums[i]);
-            if(h != null && h - nums[i] <= t || l != null && nums[i] - l <= t)  return true;
-            set.add((long)nums[i]);
-            if(i >= k) set.remove((long)nums[i - k]);
+        Map<Long, Long> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            long remapped = (long)nums[i] - Integer.MIN_VALUE;
+            long bucket = remapped / ((long)t + 1);
+            if(map.containsKey(bucket) 
+                || (map.containsKey(bucket - 1) && remapped - map.get(bucket - 1) <= t)
+                || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remapped <= t)) return true;
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remapped);
         }
         return false;
     }
