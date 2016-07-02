5e704f5919016a61f2a2a121d19482ed1b6ded37
jiao-lc <939268350@qq.com>
2016-07-02 11:47:36 +0800

    [327. Count of Range Sum][Accepted]committed by LeetCode Extension

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -1,61 +1,57 @@
 public class Solution {
-    int cnt = 0;
-    public int countRangeSum(int[] arr, int lower, int upper) {
-        int len = arr.length;
-        if(len == 0)    return 0;
-        long[] nums = new long[len + 1];
-        for(int i = 1; i <= len; i++) {  //nums[j] - nums[i] -->  arr[i] ~ arr[j - 1]
-            nums[i] = nums[i - 1] + (long)arr[i - 1];
-        }
-        count(nums, 0, len, lower, upper);
-        return cnt;
+public int countRangeSum(int[] nums, int lower, int upper) {
+    if (nums == null || nums.length == 0 || lower > upper) {
+        return 0;
     }
-    public void count(long[] nums, int start, int end, int lower, int upper) {
-        if(start >= end)    return;
-        int mid = (start + end) / 2;
-        count(nums, start, mid, lower, upper);
-        count(nums, mid + 1, end, lower, upper);
-        mergeCount(nums, start, end, lower, upper);
+
+    return countRangeSumSub(nums, 0, nums.length - 1, lower, upper);
+}
+
+private int countRangeSumSub(int[] nums, int l, int r, int lower, int upper) {
+    if (l == r) {
+        return nums[l] >= lower && nums[r] <= upper ? 1 : 0;  // base case
     }
-    
-    public void mergeCount(long[] nums, int start, int end, int lower, int upper) {
-        int mid = (start + end) / 2;
-        int left = start, right = mid + 1;
-        int start_index = start;
-        boolean round = false;
-        for(left = start_index; left <= mid; left++) {
-            for(right = mid + 1; right <= end; right++) {
-                if(nums[right] - nums[left] >= lower && !round) {
-                    round = true;
-                    start_index = left;
-                }
-                if(nums[right] - nums[left] > upper)    break;
-                if(round) cnt++;
-            }
-            round = false;
-        }
-        merge(nums, start, end);
+
+    int m = l + (r - l) / 2;
+    long[] arr = new long[r - m];  // prefix array for the second subarray
+    long sum = 0;
+    int count = 0;
+
+    for (int i = m + 1; i <= r; i++) {
+        sum += nums[i];
+        arr[i - (m + 1)] = sum; // compute the prefix array
     }
-    public void merge(long[] nums, int start, int end) {
-        long[] sortSum = new long[end - start + 1];
-        int sort_index = 0;
-        int mid = (start + end) / 2;
-        int left = start, right = mid + 1;
-        while(left <= mid && right <= end) {
-            if(nums[left] <= nums[right]) {
-                sortSum[sort_index++] = nums[left++];
-            } else {
-                sortSum[sort_index++] = nums[right++];
-            }
-        }
-        while(left <= mid) {
-            sortSum[sort_index++] = nums[left++];
-        }
-        while(right <= end) {
-            sortSum[sort_index++] = nums[right++];
-        }
-        for(int i = start; i <= end; i++) {
-            nums[i] = sortSum[i - start];
+
+    Arrays.sort(arr);  // sort the prefix array
+
+    // Here we can compute the suffix array element by element.
+    // For each element in the suffix array, we compute the corresponding
+    // "insertion" indices of the modified bounds in the sorted prefix array
+    // then the number of valid ranges sums will be given by the indices difference.
+    // I modified the bounds to be "double" to avoid duplicate elements.
+    sum = 0;
+    for (int i = m; i >= l; i--) {
+        sum += nums[i];  
+        count += findIndex(arr, upper - sum + 0.5) - findIndex(arr, lower - sum - 0.5);
+    }
+
+    return countRangeSumSub(nums, l, m, lower, upper) + countRangeSumSub(nums, m + 1, r, lower, upper) + count;
+}
+
+// binary search function
+private int findIndex(long[] arr, double val) {
+    int l = 0, r = arr.length - 1, m = 0;
+
+    while (l <= r) {
+        m = l + (r - l) / 2;
+
+        if (arr[m] <= val) {
+            l = m + 1;
+        } else {
+            r = m - 1;
         }
     }
+
+    return l;
+}
 }
