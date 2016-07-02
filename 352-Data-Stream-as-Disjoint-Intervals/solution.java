a9c8727f424fa68b756a12dda70f26c8ef3e572b
jiao-lc <939268350@qq.com>
2016-07-02 13:43:03 +0800

    [352. Data Stream as Disjoint Intervals][Accepted]committed by LeetCode Extension

--- 352-Data-Stream-as-Disjoint-Intervals/solution.java
+++ 352-Data-Stream-as-Disjoint-Intervals/solution.java
@@ -8,31 +8,97 @@
  * }
  */
 public class SummaryRanges {
-    TreeMap<Integer, Interval> tree;
+    class BSTNode {
+        Interval interval;
+        BSTNode left;
+        BSTNode right;
+        BSTNode(Interval in){
+            interval = in;
+        }
+    }
 
+    BSTNode findMin(BSTNode root) {
+        if (root == null) return null;
+        if (root.left == null ) return root;
+        else return findMin(root.left);
+    }
+
+    BSTNode remove(Interval x, BSTNode root) {
+        if (root == null) return null;
+        else if ( x == null ) return root;
+        else if (x.start > root.interval.end ) {
+            root.right = remove(x, root.right);
+        } else if (x.end < root.interval.start ) {
+            root.left = remove(x, root.left);
+        } else if ( root.left != null && root.right != null) {
+            root.interval = findMin(root.right).interval;
+            root.right = remove( root.interval, root.right);
+        } else {
+            root = ( root.left != null ) ? root.left : root.right;
+        }
+        return root;
+    }
+
+    BSTNode findKey(int val, BSTNode root) {
+        if (root == null) return null;
+        if (root.interval.start > val) {
+            return findKey(val, root.left);
+        } else if (root.interval.end < val) {
+            return findKey(val, root.right);
+        } else return root;
+    }
+
+    BSTNode addKey(int val, BSTNode root) {
+        if (root == null) {
+            root = new BSTNode( new Interval(val, val) ); 
+        } else if (root.interval.start > val) {
+            root.left = addKey(val, root.left);
+        } else if (root.interval.end < val) {
+            root.right = addKey(val, root.right);
+        }  
+        return root;
+    }
+    void inOrder(BSTNode root) {
+        if (root != null) {
+            inOrder(root.left);
+            list.add(root.interval);
+            inOrder(root.right);
+        }
+    }
+
+    /** Initialize your data structure here. */
+    BSTNode root;
+    List<Interval> list = new ArrayList();
     public SummaryRanges() {
-        tree = new TreeMap<>();
+        root = null;
     }
 
     public void addNum(int val) {
-        if(tree.containsKey(val)) return;
-        Integer l = tree.lowerKey(val);
-        Integer h = tree.higherKey(val);
-        if(l != null && h != null && tree.get(l).end + 1 == val && h == val + 1) {
-            tree.get(l).end = tree.get(h).end;
-            tree.remove(h);
-        } else if(l != null && tree.get(l).end + 1 >= val) {
-            tree.get(l).end = Math.max(tree.get(l).end, val);
-        } else if(h != null && h == val + 1) {
-            tree.put(val, new Interval(val, tree.get(h).end));
-            tree.remove(h);
+        if (root == null) {
+            root = addKey(val, root);
         } else {
-            tree.put(val, new Interval(val, val));
+            if ( findKey(val, root) != null) return;
+            BSTNode left = findKey(val-1, root);
+            BSTNode right = findKey(val+1, root);
+            if (left == null && right == null) {
+                root = addKey(val, root);
+            } else if (left != null && right == null) {
+                left.interval.end++;
+            } else if (left == null && right != null) {
+                right.interval.start--;
+            } else {
+                Interval l = left.interval;
+                int e = right.interval.end;
+                root = remove(right.interval, root);
+                l.end = e;
+            }
         }
     }
 
     public List<Interval> getIntervals() {
-        return new ArrayList<>(tree.values());
+        list.clear();
+        inOrder(root);
+        return list;
     }
 }
 
