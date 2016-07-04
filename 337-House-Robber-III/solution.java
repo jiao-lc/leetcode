eeade86c435e24d333d1d81a04a3ee7abfbe3302
jiao-lc <939268350@qq.com>
2016-07-04 09:19:25 +0800

    [337. House Robber III][Accepted]committed by LeetCode Extension

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -9,13 +9,21 @@
  */
 public class Solution {
     public int rob(TreeNode root) {
-        if(root == null)    return 0;
-        int res = root.val;
-        return Math.max(res + robChild(root.left) + robChild(root.right), robChild(root));
+        int[] res = robSub(root);
+        return Math.max(res[0], res[1]);
     }
-    
-    public int robChild(TreeNode root) {
-        if(root == null)    return 0;
-        return rob(root.left) + rob(root.right);
+ 
+    public int[] robSub(TreeNode root){
+        if(root == null)
+            return new int[2];
+ 
+        int[] left = robSub(root.left);
+        int[] right = robSub(root.right);
+ 
+        int[] res = new int[2];
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]); // do not choose current node
+        res[1] = root.val + left[0] + right[0]; // choose current node
+ 
+        return res;
     }
 }
